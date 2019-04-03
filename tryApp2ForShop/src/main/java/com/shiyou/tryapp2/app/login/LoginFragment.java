1ddbd7d864d822a4ccca357572593f0a96ed7997
lzj9527 <2414418328@qq.com>
2019-04-03 17:03:46 +0800

    基本完善3

---
+++ tryApp2ForShop/src/main/java/com/shiyou/tryapp2/app/login/LoginFragment.java
@@ -0,0 +1,382 @@
+package com.shiyou.tryapp2.app.login;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.extend.ErrorInfo;
+import android.extend.app.BaseFragment;
+import android.extend.loader.BaseParser.DataFrom;
+import android.extend.loader.Loader.CacheMode;
+import android.extend.util.AndroidUtils;
+import android.extend.util.LogUtil;
+import android.extend.util.ResourceUtil;
+import android.extend.util.ViewTools;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
+import android.webkit.WebView;
+import android.webkit.WebViewFragment;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.shiyou.tryapp2.Config;
+import com.shiyou.tryapp2.Define;
+import com.shiyou.tryapp2.RequestManager;
+import com.shiyou.tryapp2.RequestManager.RequestCallback;
+import com.shiyou.tryapp2.ResourceHelper2;
+import com.shiyou.tryapp2.ResourceHelper2.OnResourceDownloadCallback;
+import com.shiyou.tryapp2.app.MainActivity;
+import com.shiyou.tryapp2.app.MainFragment;
+import com.shiyou.tryapp2.app.product.MainIndexFragment;
+import com.shiyou.tryapp2.app.product.MainWebFragment;
+import com.shiyou.tryapp2.data.response.BaseResponse;
+import com.shiyou.tryapp2.data.response.GoodsListResponse;
+import com.shiyou.tryapp2.data.response.GoodsListResponse.GoodsItem;
+import com.shiyou.tryapp2.data.response.LoginResponse;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import static android.os.ParcelFileDescriptor.MODE_APPEND;
+
+public class LoginFragment extends BaseFragment
+{
+	EditText edit_user;
+	EditText edit_password;
+	TextView bindHint;
+
+	boolean mLoginFinished = false;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+	{
+		mLayoutResID = ResourceUtil.getLayoutId(getContext(), "login_layout");
+		View view = super.onCreateView(inflater, container, savedInstanceState);
+		ViewTools.adapterAllViewMarginInChildren(view, MainActivity.scaled);
+		ViewTools.adapterAllViewPaddingInChildren(view, MainActivity.scaled);
+		ViewTools.adapterAllTextViewTextSizeInChildren(view, MainActivity.fontScaled);
+
+		// int id = ResourceUtil.getId(getContext(), "login_box");
+		// View login_box = view.findViewById(id);
+		// ViewTools.adapterViewSize(login_box, MainActivity.scaled);
+
+		int id = ResourceUtil.getId(getContext(), "edit_user");
+		edit_user = (EditText)view.findViewById(id);
+		edit_user.setText(LoginHelper.getUserName(getContext()));
+
+		id = ResourceUtil.getId(getContext(), "edit_password");
+		edit_password = (EditText)view.findViewById(id);
+		edit_password.setText(LoginHelper.getUserPassword(getContext()));
+
+		id = ResourceUtil.getId(getContext(), "login");
+		View login = view.findViewById(id);
+		login.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				doLogin();
+			}
+		});
+
+		id = ResourceUtil.getId(getContext(), "bindHint");
+		bindHint = (TextView)view.findViewById(id);
+		bindHint.getViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener()
+		{
+			@Override
+			public void onGlobalLayout()
+			{
+				if (bindHint.getWidth() == 0)
+					return;
+				bindHint.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+				int width = bindHint.getWidth();
+				LayoutParams params = bindHint.getLayoutParams();
+				params.width = width;
+				bindHint.setLayoutParams(params);
+			}
+		});
+
+		return view;
+	}
+
+	private void doLogin()
+	{
+		if (mLoginFinished)
+			return;
+		final String user = edit_user.getText().toString();
+		if (TextUtils.isEmpty(user))
+		{
+			showToast("请输入用户名");
+			return;
+		}
+		String password = edit_password.getText().toString();
+		if (TextUtils.isEmpty(password))
+		{
+			showToast("请输入密码");
+			return;
+		}
+		showLoadingIndicator();
+		LoginHelper.login(getActivity(), user, password, new RequestCallback()
+		{
+			@Override
+			public void onRequestResult(int requestCode, long taskId, BaseResponse response, DataFrom from)
+			{
+				hideLoadingIndicator();
+				switch (response.resultCode)
+				{
+					case BaseResponse.RESULT_OK:
+						LoginResponse loginResponse = (LoginResponse)response;
+						onLoginFinished(user, loginResponse.datas.realname, loginResponse.datas.key);
+						break;
+					case LoginResponse.RESULT_UNBIND:
+						showToast(response.error);
+						showBindHint();
+						break;
+					default:
+						showToast(response.error);
+						break;
+				}
+			}
+
+			@Override
+			public void onRequestError(int requestCode, long taskId, ErrorInfo error)
+			{
+				hideLoadingIndicator();
+				showToast("很难受，网络异常: " + error.errorCode);
+			}
+		});
+		getToken();
+	}
+
+	private void showBindHint()
+	{
+		AndroidUtils.MainHandler.post(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				StringBuffer sb = new StringBuffer();
+				sb.append("您的设备还未授权，请电话联系客服告知设备号授权").append('\n');
+				sb.append("设备号: ").append(android.os.Build.SERIAL).append('\n');
+				sb.append("客服电话: ").append(Config.ServicePhone);
+				bindHint.setText(sb.toString());
+				bindHint.setVisibility(View.VISIBLE);
+			}
+		});
+	}
+
+	public void onLoginFinished(final String userName, final String realName, final String userKey)
+	{
+
+		Log.d(TAG, "onLoginFinished: userName="+LoginHelper.getUserName(getContext()));
+		Log.d(TAG, "onLoginFinished: userPassword="+LoginHelper.getUserPassword(getContext()));
+		LogUtil.v(TAG, "onLoginFinished: " + userName + "; " + realName + "; " + userKey + "; " + mLoginFinished);
+		if (mLoginFinished)
+			return;
+		mLoginFinished = true;
+		LoginHelper.onLoginFinished(getActivity(), userName, realName, userKey);
+		if (getActivity() != MainActivity.instance)
+		{
+
+			getActivity().finish();
+			if (MainFragment.instance != null)
+			{
+				MainFragment.instance.backToHomepage();
+
+				MainFragment.instance.doRefresh();
+			}
+			if (MainIndexFragment.instance != null)
+				MainIndexFragment.instance.doRefresh();
+		}
+		else
+		{
+
+//			new ResourceHelper2(getActivity(), userKey, false, false)               //171229 登录时不检查更新
+//					.checkAndDownloadResource(new OnResourceDownloadCallback()
+//					{
+//						@Override
+//						public void onDownloadFinished(Object data, boolean canceled)
+//						{
+//							showLoadingIndicator();
+//							RequestManager.loadShopLogoAndAD(getContext(), userKey, new RequestCallback()
+//							{
+//								@Override
+//								public void onRequestResult(int requestCode, long taskId, BaseResponse response,
+//										DataFrom from)
+//								{
+//									if (response != null)
+//										if (response.resultCode == BaseResponse.RESULT_OK)
+//										{
+//										}
+//										else
+//										{
+//											showToast(response.error);
+//										}
+									RequestManager.loadGoodsList(getActivity(), userKey, true, new RequestCallback()
+									{
+										@Override
+										public void onRequestResult(int requestCode, long taskId,
+												BaseResponse response, DataFrom from)
+										{
+											hideLoadingIndicator();
+											String id = "5";
+											String tag = Define.TAG_RING;
+											if (response != null && response.resultCode == BaseResponse.RESULT_OK)
+											{
+												GoodsListResponse glResponse = (GoodsListResponse)response;
+												if (glResponse.datas != null && glResponse.datas.list != null
+														&& glResponse.datas.list.length > 0)
+												{
+													for (GoodsItem item : glResponse.datas.list)
+													{
+														if (item.tag.equals(Define.TAG_RING) && item.model_info != null)
+														{
+															id = item.id;
+															tag = item.tag;
+															break;
+														}
+													}
+												}
+											}
+											BaseFragment.replace(getActivity(), new MainFragment(id, tag), false);
+											if (from != DataFrom.SERVER)
+												RequestManager.loadGoodsList(getContext(), userKey, true, null,
+														CacheMode.PERFER_NETWORK);
+										}
+
+										@Override
+										public void onRequestError(int requestCode, long taskId, ErrorInfo error)
+										{
+											onRequestResult(requestCode, taskId, null, DataFrom.SERVER);
+										}
+									});
+//								}
+
+//								@Override
+//								public void onRequestError(int requestCode, long taskId, ErrorInfo error)
+//								{
+//									showToast("网络错误: " + error.errorCode);
+//									onRequestResult(requestCode, taskId, null, DataFrom.SERVER);
+//								}
+//							});
+//						}
+//					});
+//
+// replace(MainFragment.instance, new MainWebFragment("http://www.zsa888.com/addons/ewei_shop/template/pad/default/shop/getToken.html?token="+Config.token, 0,true), false);
+//			AndroidUtils.MainHandler.post(new Runnable()
+//			{
+//				@Override
+//				public void run()
+//				{
+//					WebViewFragment webViewFragment=new WebViewFragment();
+//					webViewFragment.getWebView().loadUrl("http://www.zsa888.com/addons/ewei_shop/template/pad/default/shop/getToken.html?token="+Config.token);
+//				}
+//			});
+		}
+//		WebView webView=new WebView(getContext());
+//		webView.loadUrl("http://www.zsa888.com/addons/ewei_shop/template/pad/default/shop/getToken.html?&"+getToken());
+//		Log.d(TAG, "onLoginFinished: token="+getToken());
+	}
+	//获取toKen
+	public String getToken(){
+
+
+		FormBody formBody=new FormBody.Builder().add("username",LoginHelper.getUserName(getContext())).add("password",LoginHelper.getUserPassword(getContext())).build();
+		Request request=new Request.Builder().url("https://api.zsa888.cn/login").addHeader("Accept","application/vnd.zsmt.shop.v1+json").post(formBody).build();
+		OkHttpClient okHttpClient=new OkHttpClient();
+		okHttpClient.newCall(request).enqueue(new Callback() {
+			private String token;
+
+			@Override
+			public void onFailure(Call call, IOException e) {
+				System.out.println(e.getMessage());
+			}
+
+			@Override
+			public void onResponse(Call call, Response response) throws IOException {
+				String all = response.body().string();
+				int i = all.indexOf("access_token");
+				int j = all.indexOf("token_type");
+				Log.d(TAG, "onResponse: all=" + all);
+				Log.d(TAG, "onResponse: i=" + i);
+				Log.d(TAG, "onResponse: j=" + j);
+				try {
+					token = all.substring(i + 15, j - 3);
+					AndroidUtils.MainHandler.post(new Runnable() {
+						@Override
+						public void run() {
+//									int id=ResourceUtil.getId(getContext(),"test_token");
+//									WebView webView=(WebView) getView().findViewById(id);
+//									webView.loadUrl("http://www.zsa888.com/addons/ewei_shop/template/pad/default/shop/getToken.html?token="+token);
+							Config.token=token;
+//									replace(instance, new MainWebFragment("http://www.zsa888.com/addons/ewei_shop/template/pad/default/shop/getToken.html?token="+token, 0,true), false);
+
+//									Log.d(TAG, "run: textToken="+textView.getText());
+//                                	OkHttpClient okHttpClient1=new OkHttpClient();
+//									FormBody formBody=new FormBody.Builder().add("token",token).build();
+//									final Request request=new Request.Builder().url("http://www.zsa888.com/addons/ewei_shop/template/pad/default/shop/getToken.html").post(formBody).build();
+//									OkHttpClient okHttpClient=new OkHttpClient();
+//									okHttpClient.newCall(request).enqueue(new Callback() {
+//										@Override
+//										public void onFailure(Call call, IOException e) {
+//											Log.d(TAG, "onFailure: 222执行");
+//										}
+//
+//										@Override
+//										public void onResponse(Call call, Response response) throws IOException {
+//											String token=response.body().string();
+//
+//											Log.d(TAG, "onResponse: 222执行 token="+token);
+//										}
+//									});
+//                                    Log.d(TAG, "run: token222="+token);
+//									int id=ResourceUtil.getId(getContext(),"test_token");
+//									WebView webView= (WebView) getView().findViewById(id);
+//									webView.setWebViewClient(new WebViewClient());
+//									webView.evaluateJavascript("http://www.zsa888.com/addons/ewei_shop/template/pad/default/shop/getToken.html?token=" + token, new ValueCallback<String>() {
+//										@Override
+//										public void onReceiveValue(String value) {
+//											Log.d(TAG, "onReceiveValue: value="+value);
+//										}
+//									});
+						}
+					});
+				}catch (Exception e){
+					i=all.indexOf("data");
+					j=all.lastIndexOf("}");
+					token=all.substring(i+7,j-1);
+				}
+			}
+		});
+//			RequestManager.getToken(getContext(), LoginHelper.getUserName(getCo ntext()), LoginHelper.getUserPassword(getContext()), new RequestCallback() {
+//				@Override
+//				public void onRequestError(int requestCode, long taskId, ErrorInfo error) {
+//
+
+//				}
+//
+//				@Override
+//				public void onRequestResult(int requestCode, long taskId, BaseResponse response, DataFrom from) {
+//					TokenResponse tokenResponse=(TokenResponse)response;
+//					token=tokenResponse.tokenInfo.token;
+//
+//					Log.d(TAG, "onRequestResult: token="+token);
+//				}
+//			});
+		return  Config.token;
+	}
+}
---
+++ tryApp2ForShop/src/main/java/com/shiyou/tryapp2/app/product/OtherIndexFragment.java
@@ -0,0 +1,217 @@
+package com.shiyou.tryapp2.app.product;
+
+import android.extend.ErrorInfo;
+import android.extend.app.BaseFragment;
+import android.extend.loader.BaseParser.DataFrom;
+import android.extend.loader.BitmapLoader.DecodeMode;
+import android.extend.util.AndroidUtils;
+import android.extend.util.ResourceUtil;
+import android.extend.util.ViewTools;
+import android.extend.util.ViewTools.FitMode;
+import android.extend.widget.ExtendImageView;
+import android.extend.widget.FragmentContainer;
+import android.extend.widget.adapter.AbsAdapterItem;
+import android.extend.widget.adapter.BaseGridAdapter;
+import android.extend.widget.adapter.ScrollGridView;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView.ScaleType;
+import android.widget.TextView;
+
+import com.shiyou.tryapp2.Config;
+import com.shiyou.tryapp2.Define;
+import com.shiyou.tryapp2.RequestManager;
+import com.shiyou.tryapp2.RequestManager.RequestCallback;
+import com.shiyou.tryapp2.app.MainActivity;
+import com.shiyou.tryapp2.app.MainFragment;
+import com.shiyou.tryapp2.app.WebViewFragment;
+import com.shiyou.tryapp2.app.login.LoginHelper;
+import com.shiyou.tryapp2.data.response.BaseResponse;
+import com.shiyou.tryapp2.data.response.GoodsCategorysResponse;
+import com.shiyou.tryapp2.data.response.GoodsCategorysResponse.CategoryItem;
+import com.shiyou.tryapp2.shop.zsa.R;
+
+public class OtherIndexFragment extends BaseFragment
+{
+	private BaseGridAdapter<AbsAdapterItem> mAdapter;
+	private Button product_search;
+	private EditText product_search_number;
+	private FragmentContainer search_number;
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+	{
+		mLayoutResID = ResourceUtil.getLayoutId(getActivity(), "other_index_layout");
+		View view = super.onCreateView(inflater, container, savedInstanceState);
+		((android.extend.widget.ExtendLinearLayout)view).setInterceptTouchEventToDownward(true);
+
+		int id = ResourceUtil.getId(getContext(), "index_container");
+		ScrollGridView index_container = (ScrollGridView)view.findViewById(id);
+		index_container.setNumColumns(3);
+		int space = AndroidUtils.dp2px(getContext(), 40);
+		index_container.setVerticalDividerWidth(space);
+		index_container.setHorizontalDividerHeight(space);
+		mAdapter = new BaseGridAdapter<AbsAdapterItem>();
+		index_container.setAdapter(mAdapter);
+		id=ResourceUtil.getId(getContext(),"product_search");
+		product_search= (Button)view.findViewById(id);
+		id=ResourceUtil.getId(getContext(),"product_search_number");
+		product_search_number= (EditText)view.findViewById(id);
+		id=ResourceUtil.getId(getContext(),"search_number");
+		search_number=(FragmentContainer)view.findViewById(id);
+		search_number.setVisibility(View.GONE);
+		product_search.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				try {
+					String goodsId = product_search_number.getText().toString();
+//					int id = ResourceUtil.getId(getContext(), "token");
+//					TextView textView = (TextView) getView().findViewById(id);
+//					MainFragment.instance.addProductDetailFragmentToCurrent(goodsId, Define.TAG_RING, true, true, false, "https://api.zsa888.cn/goods/search?isSearch=true&sku=" +goodsId );
+					search_number.setVisibility(View.VISIBLE);
+					replace(getActivity(), ResourceUtil.getId(getContext(), "search_number"), new MainWebFragment(Config.BasePrefix+"/goods/search?isSearch=true&goodsType=spot&sku=" +goodsId,0),false);
+				}catch (Exception e){
+					showToast("输入的款号有误");
+				}
+				}
+
+		});
+		loadGoodsCategory();
+
+		return view;
+	}
+	private void loadGoodsCategory()
+	{
+		RequestManager.loadGoodsCategorys(getActivity(), new RequestCallback()
+		{
+			@Override
+			public void onRequestResult(int requestCode, long taskId, BaseResponse response, DataFrom from)
+			{
+				if (response.resultCode == BaseResponse.RESULT_OK)
+				{
+					GoodsCategorysResponse mGoodsCategoryResponse = (GoodsCategorysResponse)response;
+					mAdapter.clear();
+					for (CategoryItem mCategoryItem : mGoodsCategoryResponse.findZuanShiCategoryList())
+					{
+						mAdapter.addItem(new CategoryAdapterItem(mCategoryItem));
+					}
+					// mAdapter.addItem(new CategoryAdapterItem(null));
+				}
+				else
+				{
+					showToast(response.error);
+				}
+			}
+
+			@Override
+			public void onRequestError(int requestCode, long taskId, ErrorInfo error)
+			{
+				showToast("网络错误: " + error.errorCode);
+			}
+		});
+	}
+
+	private class CategoryAdapterItem extends AbsAdapterItem
+	{
+		CategoryItem mCategoryItem;
+
+		// GoodsCategoryResponse mGoodsCategory Response;
+
+		public CategoryAdapterItem(CategoryItem categoryItem)
+		{
+			// this.mGoodsCategoryResponse = mGoodsCategoryResponse;
+			this.mCategoryItem = categoryItem;
+		}
+
+		@Override
+		public View onCreateView(int position, ViewGroup parent)
+		{
+			 int layout = ResourceUtil.getLayoutId(getActivity(), "product_new_item");
+			 View view = View.inflate(getActivity(), layout, null);
+			 int id = ResourceUtil.getId(getActivity(), "image");
+			 ExtendImageView image = (ExtendImageView) view.findViewById(id);
+			 image.setAutoRecyleBitmap(true);
+			 
+			 view.setLayoutParams(new LayoutParams(450, 450));
+			 
+//			
+//			int layout = ResourceUtil.getLayoutId(getActivity(), "product_new_item");
+//			View view = View.inflate(getActivity(), layout, null);
+//			 
+//			int id = ResourceUtil.getId(getActivity(), "image");
+//			ExtendImageView image = (ExtendImageView) view.findViewById(id);
+//			image.setScaleType(ScaleType.FIT_CENTER);
+//			image.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+//			int padding = 40;
+//			image.setPadding(padding, padding, padding, padding);
+//			ViewTools.autoFitViewDimension(image, parent, FitMode.FIT_IN_WIDTH, 1);
+//			ViewTools.adapterViewPadding(image, MainActivity.scaled);
+
+			return view;
+		}
+
+		@Override
+		public void onUpdateView(View view, int position, ViewGroup parent)
+		{
+			// int id = ResourceUtil.getId(getActivity(), "image");
+			// ExtendImageView image = (ExtendImageView)view.findViewById(id);
+//			ExtendImageView image = (ExtendImageView)view;
+			
+			int id = ResourceUtil.getId(getActivity(), "image");
+			ExtendImageView image = (ExtendImageView) view.findViewById(id);
+			
+			id = ResourceUtil.getId(getActivity(), "new_icon");
+			ExtendImageView new_icon = (ExtendImageView) view.findViewById(id);
+			
+			if (mCategoryItem != null && mCategoryItem.thumb != null)
+			{
+				image.setImageDataSource(mCategoryItem.thumb.url, mCategoryItem.thumb.filemtime, DecodeMode.FIT_WIDTH);
+				if (Math.abs(mCategoryItem.getTheNewTime() - mCategoryItem.thumb.filemtime)< 60 * 60 * 24 * 3){
+					new_icon.setVisibility(View.VISIBLE);
+				}else {
+					new_icon.setVisibility(View.GONE);
+				}
+			}
+			else
+			{
+				image.setImageResource(R.drawable.gia);
+				new_icon.setVisibility(View.GONE);
+			}
+			image.startImageLoad(false);
+		}
+
+		@Override
+		public void onLoadViewResource(View view, int position, ViewGroup parent)
+		{
+
+		}
+
+		@Override
+		public void onRecycleViewResource(View view, int position, ViewGroup parent)
+		{
+			// int id = ResourceUtil.getId(getActivity(), "image");
+			// ExtendImageView image = (ExtendImageView) view.findViewById(id);
+			// image.recyleBitmapImage();
+		}
+
+		public void onItemClick(View adapterView, ViewGroup parent, View view, int position, long id)
+		{
+			if (mCategoryItem != null)
+			{
+				// MainFragment.instance.setCurrentFragmentToProductList(mCategoryItem.id, false);
+				MainFragment.instance.addFragmentToCurrent(new ProductListFragment(mCategoryItem.id, false,"spot",mCategoryItem.id,1), false);
+			}
+			else
+			{
+				String url = Config.WebGIADiamonds + "?key=" + LoginHelper.getUserKey(getContext());
+				MainFragment.instance.addWebFragmentToCurrent(url, false);
+			}
+		}
+	}
+}
